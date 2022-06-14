c7b148352243ddf4e0bf48a9314a966b72c9cbc8
Emilio <emiliothomasking@gmail.com>
2022-06-13 18:16:14 -0700

    added typing animation

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+//typing animation
+var typed = new Typed(".typing",{
+    strings:["","Web Developer","Web Designer","Blockchain Developer","Sound Engineer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
