b463ac7f8b2aff0323ec4e7dc686f9ab67bee51f
Emilio <emiliothomasking@gmail.com>
2022-06-19 14:02:05 -0700

    added pre loader animation

--- assets/script.js
+++ assets/script.js
@@ -84,3 +84,23 @@ const navTogglerBtn = document.querySelector(".nav-toggler"),
                     allSection[i].classList.toggle("open");
                 }
             }
+
+
+
+    // Page loading animation
+    $(window).on('load', function() {
+        if($('.cover').length){
+            $('.cover').parallax({
+                imageSrc: $('.cover').data('image'),
+                zIndex: '1'
+            });
+        }
+
+        $("#preloader").animate({
+            'opacity': '0'
+        }, 700, function(){
+            setTimeout(function(){
+                $("#preloader").css("visibility", "hidden").fadeOut();
+            }, 300);
+        });
+    });
